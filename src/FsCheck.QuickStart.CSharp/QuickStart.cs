8550d545e6e1c58800942ab15efa4439b87698e2
ZhangYang <yrzhang@thoughtworks.com>
2019-03-02 13:57:59 +0800

    initialize code base

---
+++ src/FsCheck.QuickStart.CSharp/QuickStart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+using FsCheck;
+using FsCheck.Xunit;
+
+
+namespace FsCheck.QuickStart.CSharp
+{
+    public class QuickStart 
+    {
+        [Property(QuietOnSuccess = true, Verbose = true)]
+        public bool ReverseReverseIsOriginal(int[] xs)
+        {
+            return xs.Reverse().Reverse().SequenceEqual(xs);
+        }
+
+        [Property(Verbose = true)]
+        public bool ReverseIsNotOriginal(int[] xs)
+        {
+            return xs.Reverse().SequenceEqual(xs);
+        }
+    }
+    
+}
---
+++ src/FsCheck.QuickStart.CSharpExamples/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace FsCheck.QuickStart.CSharpExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Prop.ForAll<int[]>(xs => xs.Reverse().Reverse().SequenceEqual(xs))
+                .Label("label 1")
+                .QuickCheck("Reverse reverse is original");
+            
+            Prop.ForAll<int[]>(xs => xs.Reverse().SequenceEqual(xs))
+                .Label("label 2")
+                .QuickCheck("Reverse is not original");
+            
+            Prop.ForAll<int>(x => false.Label("Always false")
+                    .And(Math.Abs(x) - x == 0))
+                .QuickCheck("label 3");
+            
+            Prop.ForAll<int>(x => false.Label("Always false")
+                    .And(Math.Abs(x) - x == 0))
+                .VerboseCheck("label 4");
+
+            Console.ReadKey();
+        }
+    }
+}
